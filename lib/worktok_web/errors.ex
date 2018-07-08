c8d1fec2d0650d5def4cf8d5d7744dd96db4bf10
Aleksey Gureiev <spyromus@yandex.ru>
2018-07-08 22:49:10 +0300

    Improved errors

--- lib/worktok_web/errors.ex
+++ lib/worktok_web/errors.ex
@@ -10,15 +10,11 @@ defmodule WorktokWeb.Errors do
   def full_messages(%Ecto.Changeset{} = changeset) do
     changeset
     |> Ecto.Changeset.traverse_errors(&full_message/3)
-    |> Enum.reduce([], fn {_key, errors}, acc -> acc ++ errors end)
+    |> Enum.flat_map(&elem(&1, 1))
   end
 
   defp full_message(%Ecto.Changeset{} = changeset, key, error) do
-    module_name =
-      changeset.data.__struct__
-      |> Module.split
-      |> Enum.join(".")
-
+    module_name = inspect(changeset.data.__struct__)
     key_path = "#{module_name}.#{key}"
     key_name = case Gettext.dgettext(WorktokWeb.Gettext, "schema", key_path) do
       ^key_path -> key
