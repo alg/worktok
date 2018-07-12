d3d1f4ba6ece288e12b83744f22b6dabe3d91078
Aleksey Gureiev <spyromus@yandex.ru>
2018-07-12 20:18:20 +0300

    Added new session template test

--- test/worktok_web/controllers/session_controller_test.exs
+++ test/worktok_web/controllers/session_controller_test.exs
@@ -1,6 +1,13 @@
 defmodule WorktokWeb.SessionControllerTest do
   use WorktokWeb.ConnCase
 
+  describe "new" do
+    test "should render page", %{conn: conn} do
+      conn = get conn, session_path(conn, :new)
+      assert html_response(conn, 200) =~ "Login"
+    end
+  end
+
   describe "login" do
     setup %{conn: conn} do
       user = user_fixture()
@@ -22,7 +29,7 @@ defmodule WorktokWeb.SessionControllerTest do
 
   describe "logout" do
     @tag login_as: "max"
-    test "successful", %{conn: conn, user: user} do
+    test "successful", %{conn: conn} do
       conn = delete conn, session_path(conn, :delete)
       assert get_flash(conn, :info) == "Come back soon!"
       assert redirected_to(conn) == page_path(conn, :index)
