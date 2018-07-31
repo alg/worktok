a8a447e91aaff42bb735bae6e2548f2404dbd169
Aleksey Gureiev <spyromus@yandex.ru>
2018-07-31 14:09:59 +0300

    Removed unused helper func

--- lib/worktok_web/views/profile_view.ex
+++ lib/worktok_web/views/profile_view.ex
@@ -1,9 +1,3 @@
 defmodule WorktokWeb.ProfileView do
   use WorktokWeb, :view
-
-  def join_non_blank(list, separator) do
-    list
-    |> Enum.reject(fn x -> x == nil or x == "" end)
-    |> Enum.join(separator)
-  end
 end
